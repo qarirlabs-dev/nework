28649b5461e581d6c17fb5b19bf8810abfaa6095
Moh Abdul Haq Aulia <abdul.haq.aulia@gmail.com>
2023-04-09 22:46:41 +0700

    add hero section

---
+++ resources/views/landing.blade.php
@@ -0,0 +1,62 @@
+<x-landing-layout>
+    <section class="hero w-11/12 mx-auto">
+        <div class="flex flex-row justify-between items-center">
+            <div class="flex-auto w-2/3">
+                <div class="text-5xl font-black mb-6">You bring the expertise, <br>
+                    we'll make it unforgettable.</div>
+                <div class="text-[#262626] font-normal text-2xl mb-6 text-opacity-75 leading-9">Learn directly from experts in the field of digital
+                    marketing with a <br> comprehensive yet affordable
+                    curriculum.</div>
+                <button class="bg-[#FF9500] px-10 py-2 rounded-md text-white text-lg font-bold mb-11">Upgrade Skills
+                    Now</button>
+                <div class="flex flex-row">
+                    <div class="mx-2">
+                        <div class="mb-6">Talents</div>
+                        <div class="flex items-center">
+                            <svg width="36" height="18" viewBox="0 0 36 18" fill="none"
+                                xmlns="http://www.w3.org/2000/svg">
+                                <path
+                                    d="M0 18V15.6375C0 14.5375 0.5565 13.656 1.6695 12.993C2.7815 12.331 4.225 12 6 12C6.325 12 6.6375 12.006 6.9375 12.018C7.2375 12.031 7.525 12.0625 7.8 12.1125C7.45 12.6125 7.1875 13.15 7.0125 13.725C6.8375 14.3 6.75 14.9125 6.75 15.5625V18H0ZM9 18V15.5625C9 13.9375 9.8315 12.625 11.4945 11.625C13.1565 10.625 15.325 10.125 18 10.125C20.7 10.125 22.875 10.625 24.525 11.625C26.175 12.625 27 13.9375 27 15.5625V18H9ZM29.25 18V15.5625C29.25 14.9125 29.1685 14.3 29.0055 13.725C28.8435 13.15 28.6 12.6125 28.275 12.1125C28.55 12.0625 28.8315 12.031 29.1195 12.018C29.4065 12.006 29.7 12 30 12C31.8 12 33.25 12.331 34.35 12.993C35.45 13.656 36 14.5375 36 15.6375V18H29.25ZM6 10.5C5.175 10.5 4.469 10.206 3.882 9.618C3.294 9.031 3 8.325 3 7.5C3 6.65 3.294 5.9375 3.882 5.3625C4.469 4.7875 5.175 4.5 6 4.5C6.85 4.5 7.5625 4.7875 8.1375 5.3625C8.7125 5.9375 9 6.65 9 7.5C9 8.325 8.7125 9.031 8.1375 9.618C7.5625 10.206 6.85 10.5 6 10.5ZM30 10.5C29.175 10.5 28.4685 10.206 27.8805 9.618C27.2935 9.031 27 8.325 27 7.5C27 6.65 27.2935 5.9375 27.8805 5.3625C28.4685 4.7875 29.175 4.5 30 4.5C30.85 4.5 31.5625 4.7875 32.1375 5.3625C32.7125 5.9375 33 6.65 33 7.5C33 8.325 32.7125 9.031 32.1375 9.618C31.5625 10.206 30.85 10.5 30 10.5ZM18 9C16.75 9 15.6875 8.5625 14.8125 7.6875C13.9375 6.8125 13.5 5.75 13.5 4.5C13.5 3.225 13.9375 2.156 14.8125 1.293C15.6875 0.431 16.75 0 18 0C19.275 0 20.3435 0.431 21.2055 1.293C22.0685 2.156 22.5 3.225 22.5 4.5C22.5 5.75 22.0685 6.8125 21.2055 7.6875C20.3435 8.5625 19.275 9 18 9Z"
+                                    fill="#262626" />
+                            </svg>
+                            <div class="font-bold text-3xl mx-2">1K+</div>
+                        </div>
+                    </div>
+                    <div class="mx-2">
+                        <div class="mb-6">Our grads get hired by</div>
+                        <div class="flex items-end pt-1">
+                            <img src="{{ Vite::asset('resources/image/icon/shopee.png') }}" alt="shopee"
+                                width="87" height="28" class="mx-2">
+                            <img src="{{ Vite::asset('resources/image/icon/stackearn.png') }}" alt="stackearn"
+                                width="125.21" height="28" class="mx-2">
+                            <img src="{{ Vite::asset('resources/image/icon/dentsu.png') }}" alt="dentsu"
+                                width="127" height="28" class="mx-2">
+                            <div class="flex mx-2 text-xl items-center font-medium">
+                                <span class="mr-2">View More</span>
+                                <svg width="25" height="24" viewBox="0 0 25 24" fill="none"
+                                    xmlns="http://www.w3.org/2000/svg">
+                                    <rect x="0.20755" width="24" height="24" rx="12" fill="white" />
+                                    <g clip-path="url(#clip0_5_2582)">
+                                        <path
+                                            d="M18.0157 11.5339L13.6821 7.20015C13.5584 7.07645 13.3935 7.00854 13.2177 7.00854C13.0417 7.00854 12.877 7.07654 12.7533 7.20015L12.3598 7.59371C12.2362 7.71722 12.1681 7.8822 12.1681 8.0581C12.1681 8.2339 12.2362 8.40444 12.3598 8.52795L14.888 11.0617H6.85584C6.49369 11.0617 6.20755 11.3452 6.20755 11.7074V12.2638C6.20755 12.6261 6.49369 12.9382 6.85584 12.9382H14.9166L12.3599 15.486C12.2363 15.6097 12.1682 15.7702 12.1682 15.9461C12.1682 16.1218 12.2363 16.2847 12.3599 16.4083L12.7534 16.8006C12.8771 16.9243 13.0418 16.9917 13.2178 16.9917C13.3936 16.9917 13.5585 16.9234 13.6822 16.7997L18.0158 12.4661C18.1398 12.342 18.208 12.1763 18.2076 12.0002C18.2079 11.8235 18.1398 11.6578 18.0157 11.5339Z"
+                                            fill="#FF9500" />
+                                    </g>
+                                    <defs>
+                                        <clipPath id="clip0_5_2582">
+                                            <rect width="12" height="12" fill="white"
+                                                transform="translate(6.20755 6)" />
+                                        </clipPath>
+                                    </defs>
+                                </svg>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="flex-auto w-1/3 -mr-6">
+                <img src="{{ Vite::asset('resources/image/ilustration/ilustration_1.png') }}" alt="ilustration 1">
+            </div>
+        </div>
+    </section>
+</x-landing-layout>
---
+++ resources/views/layouts/landing-layout.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>{{ config('app.name', 'Laravel') }}</title>
+
+        <!-- Fonts -->
+        <link href="https://api.fontshare.com/v2/css?f[]=satoshi@700,500,600,300,400&display=swap" rel="stylesheet">
+
+        <!-- Scripts -->
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    </head>
+    <body class="font-sans antialiased">
+        <div class="min-h-screen bg-gray-100">
+            <x-landing-nav/>
+            <!-- Page Content -->
+            <main>
+                {{ $slot }}
+            </main>
+        </div>
+    </body>
+</html>
