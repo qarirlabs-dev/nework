50656ab62853d0c5a543b3bbe03e31e99548cd11
Moh Abdul Haq Aulia <abdul.haq.aulia@gmail.com>
2023-04-29 09:38:04 +0700

    fix font size on mobile

--- resources/views/landing_page/privacy-policy.blade.php
+++ resources/views/landing_page/privacy-policy.blade.php
@@ -1,7 +1,7 @@
 <x-landing-layout>
     <div class="flex flex-col md:py-16 py-9 md:px-20 px-4 text-justify md:gap-7 gap-5">
         <h1 class="md:text-5xl text-2xl font-bold text-neutral-900">Privacy &amp; Policy</h1>
-        <p class="font-normal text-neutral-500 text-justify md:text-base text-xs md:mb-6 mb-3.5">Policy This Privacy
+        <p class="font-normal text-neutral-500 text-justify md:text-base text-base md:mb-6 mb-3.5">Policy This Privacy
             Policy explains how {{config('app.name')}} is. (“{{config('app.name')}}”, “QG”) are committed to respecting and protecting your
             privacy. We understand how important your personal data is and we will not share it with other people or
             third parties without your permission. Please read the following Privacy Policy to learn more about how we
@@ -9,9 +9,9 @@
             intended as general use information, and not your site specific information or course information. By using
             this site, services, or courses that you agree to, the uses and data obtained from you personally and course
             information, which can be used in accordance with this privacy policy.</p>
-        <h2 class="font-bold text-neutral-900 text-left md:text-xl text-sm">1. Personal Data We Get And How We Use It
+        <h2 class="font-bold text-neutral-900 text-left md:text-xl text-base">1. Personal Data We Get And How We Use It
         </h2>
-        <p class="font-normal text-neutral-500 text-justify md:text-base text-xs md:mb-6 mb-3.5">You can visit our site
+        <p class="font-normal text-neutral-500 text-justify md:text-base text-base md:mb-6 mb-3.5">You can visit our site
             without telling your personal data. We do not collect personal information from visitors to our site if they
             do not provide it as set out in this privacy policy.<br><br>If you want to get company or product
             information, or request information about special services, or provide comments related to services, you
@@ -28,17 +28,17 @@
             services that you have or wish to pay for in the future.
             <!-- -->
         </p>
-        <h2 class="font-bold text-neutral-900 text-left md:text-xl text-sm">2. Other Information We Get And How We Use It
+        <h2 class="font-bold text-neutral-900 text-left md:text-xl text-base">2. Other Information We Get And How We Use It
         </h2>
-        <p class="font-normal text-neutral-500 text-justify md:text-base text-xs md:mb-6 mb-3.5">The {{config('app.name')}} may also
+        <p class="font-normal text-neutral-500 text-justify md:text-base text-base md:mb-6 mb-3.5">The {{config('app.name')}} may also
             automatically retrieve and analyze site information regarding general usage of the site, services, and
             courses. We may look at your usage habits to find out what site features, services, and courses you
             generally use, site traffic volume, visit frequency transaction types and times, browser type, browser
             language, IP address and operating system, and statistical information. about how you use our services and
             courses. We only collect, view and analyze such site information in an amount that does not identify you
             personally.</p>
-        <h2 class="font-bold text-neutral-900 text-left md:text-xl text-sm">3. Information Explanation</h2>
-        <p class="font-normal text-neutral-500 text-justify md:text-base text-xs md:mb-6 mb-3.5">We will not share your
+        <h2 class="font-bold text-neutral-900 text-left md:text-xl text-base">3. Information Explanation</h2>
+        <p class="font-normal text-neutral-500 text-justify md:text-base text-base md:mb-6 mb-3.5">We will not share your
             personal data with third parties. This includes, but is not limited to, sharing site information with
             service providers to allow them to fulfill your requests, to instructors (which will become course
             information), and to your employer (boss) if you are registered through the {{config('app.name')}} for the business
@@ -48,22 +48,22 @@
             or for cooperation in investigations into government authorities, (b) to enforce terms of use, or (c) to
             protect the safety of our site users and our courses and services.<br>In addition, we may send your personal
             data information to future entities in a company merger, consolidation or reorganization.</p>
-        <h2 class="font-bold text-neutral-900 text-left md:text-xl text-sm">4. Data Storage</h2>
-        <p class="font-normal text-neutral-500 text-justify md:text-base text-xs md:mb-6 mb-3.5">We store site
+        <h2 class="font-bold text-neutral-900 text-left md:text-xl text-base">4. Data Storage</h2>
+        <p class="font-normal text-neutral-500 text-justify md:text-base text-base md:mb-6 mb-3.5">We store site
             information and personal data that we process for the benefit of the user as long as needed to provide
             services. We will store and use this personal information site as necessary to fulfill our legal
             obligations, resolve disputes, and enforce our agreements with users, and / or the time required by law in
             the applicable jurisdiction. Please see the section above for how the course information can be used.</p>
-        <h2 class="font-bold text-neutral-900 text-left md:text-xl text-sm">5. Updating, Correcting, And Deleting Your
+        <h2 class="font-bold text-neutral-900 text-left md:text-xl text-base">5. Updating, Correcting, And Deleting Your
             Information</h2>
-        <p class="font-normal text-neutral-500 text-justify md:text-base text-xs md:mb-6 mb-3.5">If you believe that the
+        <p class="font-normal text-neutral-500 text-justify md:text-base text-base md:mb-6 mb-3.5">If you believe that the
             {{config('app.name')}} has incorrect website information or course information that has been provided as part of the
             service, you may use the service to correct, develop, or delete that information. You can also contact us by
-            sending our customer support email at (partner{{"@".strtolower(config('app.name'))}}.com). We can only change course information that
+            sending our customer support email at (academic@qarirgenerator.com). We can only change course information that
             has been given to us: you must contact the instructor for other purposes.</p>
-        <h2 class="font-bold text-neutral-900 text-left md:text-xl text-sm">6. Confidentiality of Information &amp;
+        <h2 class="font-bold text-neutral-900 text-left md:text-xl text-base">6. Confidentiality of Information &amp;
             Consumer Services</h2>
-        <p class="font-normal text-neutral-500 text-justify md:text-base text-xs md:mb-6 mb-3.5">
+        <p class="font-normal text-neutral-500 text-justify md:text-base text-base md:mb-6 mb-3.5">
             <!-- -->{{config('app.name')}} will not provide or distribute personal information for users obtained through the use of
             the application to third parties. However, this does not include when the information is requested by the
             relevant authorities, including: the National Police of the Republic of Indonesia, certain Civil Service
@@ -74,9 +74,9 @@
             Civil Code, Law on Consumer Protection, Law on Information and Electronic Transactions and other relevant
             laws and regulations to operate and provide service systems this internet based communication.
         </p>
-        <h2 class="font-bold text-neutral-900 text-left md:text-xl text-sm">Contact the {{config('app.name')}}</h2>
-        <p class="font-normal text-neutral-500 text-justify md:text-base text-xs md:mb-6 mb-3.5">Users can contact the
-            {{config('app.name')}} regarding this Privacy Policy through our customer support at (partner{{"@".strtolower(config('app.name'))}}.com)</p>
-        <h2 class="font-bold text-neutral-900 text-left md:text-xl text-sm">Last update: 29 April 2023</h2>
+        <h2 class="font-bold text-neutral-900 text-left md:text-xl text-base">Contact the {{config('app.name')}}</h2>
+        <p class="font-normal text-neutral-500 text-justify md:text-base text-base md:mb-6 mb-3.5">Users can contact the
+            {{config('app.name')}} regarding this Privacy Policy through our customer support at (academic@qarirgenerator.com)</p>
+        <h2 class="font-bold text-neutral-900 text-left md:text-xl text-base">Last update: 29 April 2023</h2>
     </div>
 </x-landing-layout>
